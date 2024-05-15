4319d9c98e797eafb619e9d195808d8c939ceaae
SamuraiT9nka <dendi.228z@mail.ru>
2024-05-15 20:11:51 +0300

    Изменение функции Source

--- Группа Орехово/Source1.cpp
+++ Группа Орехово/Source1.cpp
@@ -1,22 +1,28 @@
+#pragma once
 #include <iostream>
 using namespace std;
 int SUM(int a, int b)
 {
-	int sum = a + b;
-	cout << sum << endl;
+	int res = a + b;
+	cout << res << endl;
+	return res;
 }
+
 int RAZN(int a, int b)
 {
-	int sum = a - b;
-	cout << sum << endl;
+	int res = a - b;
+	cout << res << endl;
+	return res;
 }
 int UMN(int a, int b)
 {
-	int sum = a * b;
-	cout << sum << endl;
+	int res = a * b;
+	cout << res << endl;
+	return res;
 }
 double DEL(int a, int b)
 {
-	int sum = a / b;
-	cout << sum << endl;
+	int res = a / b;
+	cout << res << endl;
+	return res;
 }
