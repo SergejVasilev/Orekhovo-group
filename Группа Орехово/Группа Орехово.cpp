0022c141dacfed6fd731cee310f0b9d9747e372e
Дамир <kamalovdamir01@gmail.com>
2024-05-15 19:14:43 +0300

    исправил ошибку

--- Группа Орехово/Группа Орехово.cpp
+++ Группа Орехово/Группа Орехово.cpp
@@ -6,15 +6,11 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");   // подключение русского языка
+
     std::cout << "Hello World!\n";
     cout << "всем хай от Таня.И.А" << endl; //мой комент
     cout << "Привет!!!!  Еремеев С.Н." << endl; //мой комент
     cout << "Всем привет, от Артёма П." << endl; //мой комент
-
-
-}
-
-
 
     return 0; //добавил короче чето я Дамир
 }
