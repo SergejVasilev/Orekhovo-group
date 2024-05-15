2d8ffaa68b119a182424900974d690afaf1485bb
tanyaivleva <tetefone2008@mail.ru>
2024-05-15 19:23:35 +0300

    ФИКС

--- Группа Орехово/Группа Орехово.cpp
+++ Группа Орехово/Группа Орехово.cpp
@@ -7,6 +7,9 @@ int main()
     setlocale(LC_ALL, "Russian");   // подключение русского языка
     std::cout << "Hello World!\n";
     cout << "всем хай от Таня.И.А" << endl; //мой комент
+
+
+
 }
 
 
