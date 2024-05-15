d1f44c21275ca59bdc4d030b5af907f5da134aef
SergeiEremeev <sergeieremeev88@gmail.com>
2024-05-15 19:03:29 +0300

    Еремеев

--- Группа Орехово/Группа Орехово.cpp
+++ Группа Орехово/Группа Орехово.cpp
@@ -7,6 +7,7 @@ int main()
     setlocale(LC_ALL, "Russian");   // подключение русского языка
     std::cout << "Hello World!\n";
     cout << "всем хай от Таня.И.А" << endl; //мой комент
+    cout << "Привет!!!!  Еремеев С.Н." << endl; //мой комент
 }
 
 
