3bf363eb46951976739790d29d1c21a959584c14
SergejVasilev <vsv2978@yandex.ru>
2024-05-15 23:31:07 +0300

    Добавил файл relise func "RF"

---
+++ Группа Орехово/RF.cpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+double SUM(double x, double y)
+{
+	double res = x + y;
+	cout << res << endl;
+	cout << "-----------------------------------------" << endl;
+	return res;
+}
+double RAZN(double x, double y)
+{
+	double res = x - y;
+	cout << res << endl;
+	cout << "-----------------------------------------" << endl;
+	return res;
+}
+double UMN(double x, double y)
+{
+	double res = x * y;
+	cout << res << endl;
+	cout << "-----------------------------------------" << endl;
+	return res;
+}
+double DEL(double x, double y)
+{
+	double res = x / y;
+	cout << res << endl;
+	cout << "-----------------------------------------" << endl;
+	return res;
+}
+void menu()
+{
+	cout << "\t|---------------------------|\n"; //вывод текста
+	cout << "\t|           Меню            |\n"; //вывод текста
+	cout << "\t|      1- сложение   +      |\n"; //вывод текста
+	cout << "\t|      2 - вычитание -      |\n"; //вывод текста
+	cout << "\t|      3 - умножение *      |\n"; //вывод текста
+	cout << "\t|      4 - деление   /      |\n"; //вывод текста
+	cout << "\t|      5 - выход            |\n"; //вывод текста
+	cout << "\t|---------------------------|\n\n"; //вывод текста
+}
