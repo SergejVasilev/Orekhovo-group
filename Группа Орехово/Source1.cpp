d9b976d6668a62f8410d29bfc5642a8f4e4bcb23
SamuraiT9nka <dendi.228z@mail.ru>
2024-05-15 19:36:46 +0300

    Фиксация изменений

---
+++ Группа Орехово/Source1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void SUM(int a, int b)
+{
+	int sum = a + b;
+	cout << sum << endl;
+}
+void RAZN(int a, int b)
+{
+	int sum = a - b;
+	cout << sum << endl;
+}
+void UMN(int a, int b)
+{
+	int sum = a * b;
+	cout << sum << endl;
+}
+void DEL(int a, int b)
+{
+	int sum = a / b;
+	cout << sum << endl;
+}
