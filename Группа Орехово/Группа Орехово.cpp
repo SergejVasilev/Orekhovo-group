c0d5b71c5ab6c5725fee8c8bc5019dae66474e41
Дамир <kamalovdamir01@gmail.com>
2024-05-15 19:41:05 +0300

    убрал приветы

--- Группа Орехово/Группа Орехово.cpp
+++ Группа Орехово/Группа Орехово.cpp
@@ -9,11 +9,6 @@ int main()
     SetConsoleCP(1251);           //
     SetConsoleOutputCP(1251);     //3 строки для русского языка
 
-    std::cout << "Hello World!\n";
-    cout << "всем хай от Таня.И.А" << endl; //мой комент
-    cout << "Привет!!!!  Еремеев С.Н." << endl; //мой комент
-    cout << "Всем привет, от Артёма П." << endl; //мой комент
-
     int a = 150;
     int b = 200;
     int res;
