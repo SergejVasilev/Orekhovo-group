a5737b43ea440cb3868dd289b8c6f0451db76479
SergejVasilev <vsv2978@yandex.ru>
2024-05-15 23:15:02 +0300

    вывел меню в функцию

--- Группа Орехово/Header.h
+++ Группа Орехово/Header.h
@@ -2,7 +2,7 @@
 #include <iostream>
 //#include "RF.cpp"
 using namespace std;
-
+void menu(); // меню для 
 
 double SUM(double x, double y);
 double RAZN(double x, double y);
@@ -33,4 +33,15 @@ double DEL(double x, double y)
 	double res = x / y;
 	cout << res << endl;
 	return res;
+}
+void menu()
+{
+cout << "\t|---------------------------|\n"; //вывод текста
+	cout << "\t|           Меню            |\n"; //вывод текста
+	cout << "\t|      1- сложение   +      |\n"; //вывод текста
+	cout << "\t|      2 - вычитание -      |\n"; //вывод текста
+	cout << "\t|      3 - умножение *      |\n"; //вывод текста
+	cout << "\t|      4 - деление   /      |\n"; //вывод текста
+	cout << "\t|      5 - выход            |\n"; //вывод текста
+	cout << "\t|---------------------------|\n\n"; //вывод текста
 }
