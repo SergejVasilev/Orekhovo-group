64bdccbb56693dbafa3e5d58e94e4acdf4272d73
Дамир <kamalovdamir01@gmail.com>
2024-05-15 19:09:02 +0300

    Я дабавиль ретюрн

--- Группа Орехово/Группа Орехово.cpp
+++ Группа Орехово/Группа Орехово.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <windows.h>        // Подключение библеотеку ввода - вывода
+
 using namespace std;
 
 int main()
@@ -8,6 +9,6 @@ int main()
     std::cout << "Hello World!\n";
     cout << "всем хай от Таня.И.А" << endl; //мой комент
     cout << "Привет!!!!  Еремеев С.Н." << endl; //мой комент
+
+    return 0; //добавил короче чето я Дамир
 }
-
-
